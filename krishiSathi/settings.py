ea3486d331474b19c03246b028b17a5febd11b94
LearnerAnisha <anishagautam012@gmail.com>
2025-11-09 18:50:39 +0545

    made authentication app

--- krishiSathi/settings.py
+++ krishiSathi/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'authentication',
+
+    # Third-aprty
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
