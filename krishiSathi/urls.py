979b50123a6cd5d633049ddc83e354f466c4bb6f
LearnerAnisha <anishagautam012@gmail.com>
2025-11-29 20:19:56 +0545

    added swagger

--- krishiSathi/urls.py
+++ krishiSathi/urls.py
@@ -16,9 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Krishi Sathi API",
+        default_version='v1',
+        description="API documentation for KrishiSathi",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/auth/", include("authentication.urls")),
+   path('swagger<format>/', schema_view.without_ui(), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('admin/', admin.site.urls),
+   path("api/auth/", include("authentication.urls")),
+   
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
