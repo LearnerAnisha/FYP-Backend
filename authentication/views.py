8f6a1bcbe8ac92b0c6b5ff3a43e6f2ed447af81c
LearnerAnisha <anishagautam012@gmail.com>
2025-11-29 21:34:22 +0545

    added login

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,33 @@
-from rest_framework import generics
+from rest_framework import generics,status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,LoginSerializer
 from .models import User
-
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+            "user": {
+                "id": user.id,
+                "full_name": user.full_name,
+                "email": user.email,
+                "phone": user.phone,
+            }
+        }, status=status.HTTP_200_OK)
