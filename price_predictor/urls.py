c156b63afdabc8f468b23c8a8a7e5277ffac73bf
LearnerAnisha <anishagautam012@gmail.com>
2025-12-04 11:54:55 +0545

    made analysis api

--- price_predictor/urls.py
+++ price_predictor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FetchMarketPriceAPIView,MarketPriceListAPIView
+from .views import FetchMarketPriceAPIView,MarketPriceListAPIView,MarketPriceAnalysisAPIView
 
 urlpatterns = [
     path("fetch-market-prices/", FetchMarketPriceAPIView.as_view(), name="fetch-market-prices"),
     path("prices/", MarketPriceListAPIView.as_view(), name="prices-list"),
+    path("analysis/", MarketPriceAnalysisAPIView.as_view()),
 ]
